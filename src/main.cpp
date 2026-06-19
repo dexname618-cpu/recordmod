a2e0e53f05c0d6e79be088bd09b6e74a1917d933
dexname618-cpu <dexname618@gmail.com>
2026-06-20 00:21:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 
 using namespace geode::prelude;
 
@@ -22,26 +22,25 @@ static std::string buildPickupString(float x, int itemID, int count) {
            "449,1;";
 }
 
-class $modify(MyGameLayer, GJBaseGameLayer) {
+class $modify(MyPlayerObject, PlayerObject) {
 
     struct Fields {
         bool isHolding = false;
     };
 
-    void pushButton(int button, bool player1) {
-        GJBaseGameLayer::pushButton(button, player1);
+    void pushButton(PlayerButton button) {
+        PlayerObject::pushButton(button);
 
-        if (!player1 || button != 1) return;
+        if (button != PlayerButton::Jump) return;
         if (!Mod::get()->getSettingValue<bool>("enabled")) return;
 
-        // Перевіряємо що ми в playtest редактора
         auto* editor = LevelEditorLayer::get();
         if (!editor) return;
 
         if (m_fields->isHolding) return;
         m_fields->isHolding = true;
 
-        float px   = m_player1->getPositionX();
+        float px   = this->getPositionX();
         int itemID = (int)Mod::get()->getSettingValue<int64_t>("pickup-item-id");
         int count  = (int)Mod::get()->getSettingValue<int64_t>("pickup-count-press");
 
@@ -49,10 +48,10 @@ class $modify(MyGameLayer, GJBaseGameLayer) {
         editor->createObjectsFromString(buildPickupString(px, itemID, count), true, true);
     }
 
-    void releaseButton(int button, bool player1) {
-        GJBaseGameLayer::releaseButton(button, player1);
+    void releaseButton(PlayerButton button) {
+        PlayerObject::releaseButton(button);
 
-        if (!player1 || button != 1) return;
+        if (button != PlayerButton::Jump) return;
         if (!Mod::get()->getSettingValue<bool>("enabled")) return;
 
         auto* editor = LevelEditorLayer::get();
@@ -61,7 +60,7 @@ class $modify(MyGameLayer, GJBaseGameLayer) {
         if (!m_fields->isHolding) return;
         m_fields->isHolding = false;
 
-        float px   = m_player1->getPositionX();
+        float px   = this->getPositionX();
         int itemID = (int)Mod::get()->getSettingValue<int64_t>("pickup-item-id");
         int count  = (int)Mod::get()->getSettingValue<int64_t>("pickup-count-release");
 
